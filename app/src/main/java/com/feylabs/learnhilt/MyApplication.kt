222d018c56b128c6749175a65dcbcc1d2bb7262b
henryaugusta <henryaugusta4@gmail.com>
2022-02-25 07:58:42 +0700

    fix dependency

---
+++ app/src/main/java/com/feylabs/learnhilt/MyApplication.kt
@@ -0,0 +1,13 @@
+package com.feylabs.learnhilt
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication : Application() {
+
+
+    // field injection
+
+    // constructore injection
+}
