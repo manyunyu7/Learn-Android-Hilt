bfdbb4160b3081e1637435c1e77ebf7b05042b28
henryaugusta <henryaugusta4@gmail.com>
2022-02-25 07:59:02 +0700

    finish field injection

--- app/src/main/java/com/feylabs/learnhilt/MainActivity.kt
+++ app/src/main/java/com/feylabs/learnhilt/MainActivity.kt
@@ -2,10 +2,41 @@ package com.feylabs.learnhilt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.feylabs.learnhilt.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    @Inject
+    lateinit var someClass: SomeClass
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.btnClickMe.setOnClickListener {
+            Toast.makeText(this, someClass.greet("Razky"), Toast.LENGTH_SHORT).show()
+        }
     }
+}
+
+class SomeClass
+@Inject
+constructor(
+    val otherClass: OtherClass
+) {
+    fun greet(name: String): String {
+        return "Hello $name, you are ${otherClass.getWord()}"
+    }
+}
+
+class OtherClass
+@Inject
+constructor() {
+    fun getWord(): String = arrayOf("Beautiful", "Great", "Wonderful").random()
 }
